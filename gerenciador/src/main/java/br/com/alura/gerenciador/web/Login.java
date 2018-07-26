a957156ae91700c699423a37a318589775ce3c52
Marcos Moskorz <mmoskorz@gmail.com>
2018-07-26 20:32:16 -0300

    Todo o curso foi realizado antes de eu usar git

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Login.java
@@ -0,0 +1,34 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.alura.gerenciador.Tarefa;
+import br.com.alura.gerenciador.Usuario;
+import br.com.alura.gerenciador.dao.UsuarioDAO;
+
+public class Login implements Tarefa {
+
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		Usuario usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
+		if (usuario == null) {
+			return "/WEB-INF/paginas/loginInvalido.html";
+		} else {
+			HttpSession session = request.getSession();
+			session.setAttribute("usuarioLogado", usuario);
+			request.setAttribute("emailUsuario", usuario.getEmail());
+			return "/WEB-INF/paginas/loginValido.jsp";
+
+		}
+	}
+}
